65d98dbf7a39f3c91d907845b5a1ccac9f7b29b3
Falak-Chudasama <falakchudasama7766@gmail.com>
2026-02-15 16:07:49 +0530

    Basic transmitter [no lora]

---
+++ transmitter-arduino-uno/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+#define BUTTON_PIN 5
+#define BUZZER_PIN 6
+#define LED_PIN 7
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void buzz(int howManyTimes) {
+  for (int i = 0; i < howManyTimes; i++) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    digitalWrite(LED_PIN, HIGH);
+    delay(200);
+    digitalWrite(BUZZER_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
+    delay(200);
+  }
+}
+
+void loop() {
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    buzz(15);
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
+  }
+}
